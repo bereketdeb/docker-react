ac7d5e90dd230e4ab5b4e9a89b0ca030bf82f190
bereket <bfrezghi81@gmail.com>
2019-11-10 19:09:46 -0600

    text changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React after Angular
+          Learn React after Angular group work
         </a>
       </header>
     </div>
